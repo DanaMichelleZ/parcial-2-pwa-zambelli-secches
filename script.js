1af37c02b70b5b124604d8655d1a5829da295233
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-06-23 11:06:35 -0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 // script.js xd
 
+document.addEventListener('DOMContentLoaded', () => {
+    const listaBebidas = document.querySelector('.lista-bebidas');
+
+
+
 // Pego el link asi no lo pierdo :v
 const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+async function obtenerBebidas() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        mostrarBebidas(data.drinks);
+    } catch (error) {
+        console.error('Error, no estas lo suficientemente ebrio', error);
+        listaBebidas.innerHTML = '<p>Error al cargar las bebidas. Toma un shot mas e intenta nuevamente más tarde.</p>';
+    }
+}
+
+function mostrarBebidas(bebidas) {
+    if (bebidas) {
+        listaBebidas.innerHTML = bebidas.map(bebida => `
+            <div class="bebida">
+                <h2>${bebida.strDrink}</h2>
+                <img src="${bebida.strDrinkThumb}" alt="${bebida.strDrink}">
+                <p>${bebida.strInstructions}</p>
+            </div>
+        `).join('');
+    } else {
+        listaBebidas.innerHTML = '<p>No se encontraron bebidas que te pongan mas ebrio.</p>';
+    }
+}
+
+obtenerBebidas();
